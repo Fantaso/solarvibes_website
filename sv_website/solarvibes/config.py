141d222275e56e54bf8a564c03420096c68f15ac
Takahiko Okada <takahikookada@gmail.com>
2018-07-28 15:19:14 +0200

    config modified condition added

--- sv_website/solarvibes/config.py
+++ sv_website/solarvibes/config.py
@@ -1,8 +1,17 @@
+
+import os
 from os import environ
 
 class Config:
 
-    ########## GENERAL ##########
-    DEBUG = True
-    SECRET_KEY = 'jhagdsiua6t87dgvbiu^R&%UDFVIUF^D*%^I&C'
+########## GENERAL ##########
+  SECRET_KEY = 'jhagdsiua6t87dgvbiu^R&%UDFVIUF^D*%^I&C'
+
+  if os.environ.get('ENV')== 'production':
+    app.config['DEBUG']= False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+  else:
     SQLALCHEMY_DATABASE_URI = 'postgresql://fantaso:4oZe0EPTgV@localhost/fantaso'
+    app.config['DEBUG']= True
+
+  app.run(port=3000)
