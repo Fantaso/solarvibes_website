a8ec10a5724b53c2a1771a48ae96eddcab7ba138
Takahiko Okada <takahikookada@gmail.com>
2018-07-28 16:00:17 +0200

    ENV variables changed

--- solarvibes/config.py
+++ solarvibes/config.py
@@ -4,5 +4,5 @@ import os
 class Config:
 
 ########## GENERAL ##########
-  SECRET_KEY = 'jhagdsiua6t87dgvbiu^R&%UDFVIUF^D*%^I&C'
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+  SECRET_KEY = os.environ.get('SECRET_KEY')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
