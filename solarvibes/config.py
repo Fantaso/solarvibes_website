f1c9957ba239924feb1d48199fdf0f391ac7aa36
Takahiko Okada <takahikookada@gmail.com>
2018-07-28 15:32:40 +0200

    config condition removed

--- solarvibes/config.py
+++ solarvibes/config.py
@@ -6,12 +6,5 @@ class Config:
 
 ########## GENERAL ##########
   SECRET_KEY = 'jhagdsiua6t87dgvbiu^R&%UDFVIUF^D*%^I&C'
-
-  if os.environ.get('ENV')== 'production':
-    app.config['DEBUG']= False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-  else:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://fantaso:4oZe0EPTgV@localhost/fantaso'
-    app.config['DEBUG']= True
-
-  app.run(port=3000)
+  app.config['DEBUG']= False
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
