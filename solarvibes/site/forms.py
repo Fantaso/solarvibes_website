1f03756a145e9109f8dbc24c7e4114f7dc04c11b
Takahiko Okada <takahikookada@gmail.com>
2018-08-30 18:57:51 +0200

    every box fixed

--- solarvibes/site/forms.py
+++ solarvibes/site/forms.py
@@ -8,13 +8,13 @@ class EmailForm(FlaskForm):
 	email 	= StringField('Email', validators=[DataRequired(), Length(min=5, max=30, message=None), Email()])
 
 class EmailAndTextForm(EmailForm):
-  first_name = TextAreaField('First Name', validators=[DataRequired(), Length(min=-1, max=30, message='Maximum characters: 30')])
-  last_name = TextAreaField('Last Name', validators=[DataRequired(), Length(min=-1, max=30, message='Maximum characters: 30')])
-  email = TextAreaField('Email', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
-  country = TextAreaField('Country', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
-  landsize = TextAreaField('Landsize(ha)', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
-  msg = TextAreaField('Message', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
+  first_name = StringField('First Name', validators=[DataRequired(), Length(min=-1, max=30, message='Maximum characters: 30')])
+  last_name = StringField('Last Name', validators=[DataRequired(), Length(min=-1, max=30, message='Maximum characters: 30')])
+  email = StringField('Email', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
+  country = StringField('Country', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
+  landsize = StringField('Landsize(ha)', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
+  msg = StringField('Message', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
 
 class ContactUsForm(FlaskForm):
   name 	= StringField(label='Fullname', validators=[DataRequired(), Length(min=3, max=30, message=None)])
-  email = TextAreaField('Email', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
+  email = StringField('Email', validators=[DataRequired(), Length(min=-1, max=50, message='Maximum characters: 50')])
